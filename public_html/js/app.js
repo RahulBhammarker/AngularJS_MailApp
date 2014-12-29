2718e95e860cf7af0e00af57d26db90e104b90a0
rahul <rahul@rahul-Inspiron-5520>
2014-12-29 15:00:22 +0530

    Home page layout and partial work done

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,21 +1,54 @@
-var app = angular.module('app',['ngRoute']);
+var app = angular.module('app', ['ngRoute']);
 
-app.config(function($routeProvider){
- $routeProvider.when('/',{
-  templateUrl:"partials/home.html",
+app.config(function ($routeProvider) {
+ $routeProvider.when('/', {
+  templateUrl: "partials/home.html",
   controller: 'HomeController'
  })
-  .when('/settings',{
-   templateUrl:"partials/settings.html",
-   controller:'SettingsController'
+  .when('/settings', {
+   templateUrl: "partials/settings.html",
+   controller: 'SettingsController'
   })
-  .otherwise({redirectTo:'/'});
+  .otherwise({redirectTo: '/'});
 });
 
-app.controller('HomeController',function($scope){
- $scope.name = "Sorry..!! Controller Under Construction";
+app.controller('HomeController', function ($scope) {
+ $scope.selectedMail;
+
+ $scope.setSelectedMail = function (mail) {
+  $scope.selectedMail = mail;
+ };
+
+ $scope.isSelected = function (mail) {
+  if ($scope.selectedMail) {
+   return $scope.selectedMail === mail;
+  }
+ };
 });
 
-app.controller('SettingsController',function($scope){
+app.controller('MailListingController',function ($scope, $http) {
+  $http.get('json/mails.json').success(function (data) {
+   $scope.email = data;
+  });
+ });
+
+app.controller('ContentController', function ($scope) {
+ $scope.showingReply = false;
+ 
+ $scope.reply = {};
+ 
+ $scope.toggleReplyForm = function(){
+  $scope.showingReply = !$scope.showingReply;
+  $scope.reply.to = $scope.selectedMail.to;
+  $scope.reply.body = "\n\n\n***-------------------------------------------***\n\n" + $scope.selectedMail.body;
+ };
+ 
+ $scope.$watch('selectedMail',function(evt){
+  $scope.showingReply = false;
+  $scope.reply = {};
+ });
+});
+
+app.controller('SettingsController', function ($scope) {
  $scope.name = "settingscontrollers";
 });
